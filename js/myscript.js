1681fe0ee4a658579c293ca4fac059dc6f53674f
miura <miura@qu-bic.jp>
2020-11-26 17:38:16 +0900

    トップ：作成中（pc）

--- js/myscript.js
+++ js/myscript.js
@@ -18,9 +18,16 @@ const tabletwidth = 1025;
 
 $(function () {
 	$("body").removeClass("preload");
-// JQueryの範囲
+	// JQueryの範囲
 
-
+	var rellax = new Rellax('.rellax', {
+		// center: true
+		callback: function (position) {
+			// callback every position change
+			console.log(position);
+		},
+		breakpoints: [576, 768, 1024]
+	});
 
 
 
